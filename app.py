d25c1d606de68594e8e567af0d73c327024770e4
PiyushG1816 <pg.piyushgpt18@gmail.com>
2024-11-25 13:38:33 +0530

    app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def getresult(img):
 
 @app.route('/',methods=['GET'])
 def index():
-   return render_template('test.html')
+   return render_template('index.html')
 
 @app.route('/predict', methods=['GET','POST'])
 def upload():
